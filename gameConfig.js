30f4cc2c9e61faf286063189a34f55f180fcc1be
诸葛甜甜 <14919287+zgTiantian@user.noreply.gitee.com>
2025-12-02 17:40:27 +0800

    add

--- gameConfig.js
+++ gameConfig.js
@@ -1,10 +1,95 @@
-export const GAME_CONFIG = {
-  gameData: [
-            { goldenKey: 0, diamond: 0, rareEquipment: 0, energyCore: 0, money: 0, otherItems: '', saved: false },
-            { goldenKey: 0, diamond: 0, rareEquipment: 0, energyCore: 0, money: 0, otherItems: '', saved: false },
-            { goldenKey: 0, diamond: 0, rareEquipment: 0, energyCore: 0, money: 0, otherItems: '', saved: false },
-            { goldenKey: 0, diamond: 0, rareEquipment: 0, energyCore: 0, money: 0, otherItems: '', saved: false },
-            { goldenKey: 0, diamond: 0, rareEquipment: 0, energyCore: 0, money: 0, otherItems: '', saved: false },
-            { goldenKey: 0, diamond: 0, rareEquipment: 0, energyCore: 0, money: 0, otherItems: '', saved: false }
-          ]
+window.GAME_CONFIG = {
+  goodsData: [
+    {name: '金樽', value: 'jinzun'},
+    {name: '青铜当卢', value: 'danglu'},
+    {name: '壁画碎片', value: 'suipian'},
+    {name: '金鳞', value: 'jinlin'},
+    {name: '虎符', value: 'hufu'},
+    {name: '幽灵小熊猫/土拨鼠', value: 'youling'},
+    {name: '雪莲', value: 'xuelian'},
+    {name: '鬼玺', value: 'guixi'},
+    {name: '白玉杯', value: 'baiyubei'},
+    {name: '九色神像鹿', value: 'shenlu'},
+    {name: '天下第一剑', value: 'dajian'},
+    {name: '祈愿圣杯', value: 'shengbei'}
+  ],
+  rewordData: [
+    {
+      name: '六六大顺', tip:'累计带出6朵雪莲和6个鬼玺', value: 6, condition: (game)=>{
+        game.xuelian>=6 && game.guixi>=6
+      }, show: (game)=>{
+        return `鬼玺：<span>${game.guixi}</span>，雪莲：<span>${game.xuelian}</span>`
+      }
+    },
+    {
+      name: '金樽当卢大法', tip:'累计带出10个金樽/当卢', value: 2, condition: (game)=>{
+        game.jinzun + game.danglu >= 10
+      }, show: (game)=>{
+        return `金樽：<span>${game.jinzun}</span>，青铜当卢：<span>${game.danglu}</span>`
+      }
+    },
+    {
+      name: '金鱼虎符万岁', tip:'累计带出10个金鱼/虎符', value: 2, condition: (game)=>{
+        game.jinyu + game.hufu >= 10
+      }, show: (game)=>{
+        return `金鳞<span>${game.jinyu}</span>，虎符：<span>${game.hufu}</span>`
+      }
+    },
+    {
+      name: '拼凑碎片', tip:'累计带出6个壁画碎片', value: 2, condition: (game)=>{
+        game.suipian >= 6
+      }, show: (game)=>{
+        return `壁画碎片：<span>${game.suipian}</span>`
+      }
+    },
+    {
+      name: '幽灵在哪里', tip:'累计带出6个幽灵小熊猫/土拨鼠', value: 2, condition: (game)=>{
+        game.youling >= 6
+      }, show: (game)=>{
+        return `幽灵小熊猫/土拨鼠：<span>${game.youling}</span>`
+      }
+    },
+    {
+      name: '白玉杯来', tip:'每带出一个白玉杯', count :1, value: 'baiyubei', show: (game)=>{
+        return `白玉杯：<span>${game.baiyubei}</span>`
+      }
+    },
+    {
+      name: '神鹿降临', tip:'每带出一只九色鹿', count :1, value: 'shenlu', show: (game)=>{
+        return `九色神像鹿：<span>${game.shenlu}</span>`
+      }
+    },
+    {
+      name: '大剑爽吃', tip:'每带出一把天下第一剑', count: 2, value: 'dajian', show: (game)=>{
+        return `天下第一剑：<span>${game.dajian}</span>`
+      }
+    },
+    {
+      name: '圣杯祈福', tip:'每带出一个祈愿圣杯', count: 5, value: 'shengbei', show: (game)=>{
+        return `祈愿圣杯：<span>${game.shengbei}</span>`
+      }
+    },
+    {
+      name: '点石成金', tip:'每遇到一个宝藏房', count :1, value: 'bz', show: (game)=>{
+        return `宝藏房：<span>${game.bz}</span>`
+      }
+    },
+    {
+      name: '百万撤离', tip:'每百万撤离一把', count :1, value: 'bwMoney', show: (game)=>{
+        return `百万撤离：<span>${game.bwMoney}</span>`
+      }
+    },
+    {
+      name: '垃圾走开', tip:'每25万以下撤离一把', count :1, value: 'ljMoney', show: (game)=>{
+        return `25万以下撤离：<span>${game.ljMoney}</span>`
+      }
+    },
+    {
+      name: '五福临门', tip:'累计五把五十万撤离（50w-70w）', value :5, condition: (game)=>{
+        game.wsMoney >= 5
+      }, show: (game)=>{
+        return `50万撤离：<span>${game.wsMoney}</span>`
+      }
+    },
+  ]
 }
--- gameMethods.js
+++ gameMethods.js
@@ -1,5 +1,3 @@
-import { method } from "lodash";
-
 window.Game = {
   computed: {
     // 当前游戏数据
@@ -27,16 +25,7 @@ window.Game = {
           return this.gameData.reduce((total, game) => total + Number(game.money || 0), 0);
         }
   },
-  method:  {
-   saveCurrentGame() {
-          this.gameData[this.gameIndex].saved = true;
-          
-          // 检测彩蛋
-          this.checkEggs();
-          
-          // 提示保存成功
-          this.$message.success(`第 ${this.gameIndex + 1} 局数据保存成功！`);
-        },
+  methods:  {
         // 上一局
         prevGame() {
           if (this.gameIndex > 0) {
@@ -45,46 +34,106 @@ window.Game = {
         },
         // 下一局
         nextGame() {
-          if (this.gameIndex < 5) {
+          if (this.gameIndex < this.defaultSize-1) {
             this.gameIndex++;
           }
         },
-        // 检测彩蛋
-        checkEggs() {
-          const currentGame = this.currentGame;
-          const triggeredEggs = this.eggRules.filter(rule => rule.condition(currentGame));
-          
-          if (triggeredEggs.length > 0) {
-            this.totalEggs += triggeredEggs.length;
-            // 显示彩蛋动画
-            triggeredEggs.forEach(egg => {
-              this.showEggAnimation(egg);
-            });
-          }
-        },
         // 显示彩蛋动画
-        showEggAnimation(egg) {
+        getReword(egg) {
+          let allCount = 0
+          let fitData= []
+          const gameData = this.getGameData()
+          console.log(gameData)
+          this.gameData = this.getGameData()
+          this.rewordData.forEach(item=>{
+            if(item.condition){
+              if(item.condition(gameData)){
+                allCount+=item.value
+                fitData.push({
+                  ...item,
+                  rewordCount: item.value
+                })
+              }
+            }else{
+              if(this.gameData[item.value]>0){
+                allCount+=this.gameData[item.value]*item.count
+                fitData.push({
+                  ...item,
+                  rewordCount: this.gameData[item.value]*item.count
+                })
+              }
+            }
+          })
+
           const eggContainer = this.$refs.eggContainer;
           
           // 创建彩蛋元素
           const eggElement = document.createElement('div');
           eggElement.className = 'egg';
           // 随机位置
-          eggElement.style.left = `${Math.random() * 80 + 10}%`;
+          eggElement.style.left = `20px`;
           eggContainer.appendChild(eggElement);
           
           // 创建彩蛋提示
           const toastElement = document.createElement('div');
           toastElement.className = 'egg-toast';
-          toastElement.innerHTML = `<strong>🎉 ${egg.name} 🎉</strong><br>${egg.description}`;
-          toastElement.style.left = `${Math.random() * 70 + 15}%`;
+          toastElement.innerHTML = `<strong>🎉 恭喜板板 🎉</strong><br>奖励局数：${allCount}把`;
+          // toastElement.style.left = `20px`;
           eggContainer.appendChild(toastElement);
           
           // 动画结束后移除元素
-          setTimeout(() => {
-            eggElement.remove();
-            toastElement.remove();
-          }, 3000);
+          // setTimeout(() => {
+          //   eggElement.remove();
+          //   toastElement.remove();
+          // }, 3000);
+        },
+
+        getGameData(){
+          let params = {
+            bz: 0,
+            bwMoney: 0,
+            ljMoney: 0,
+            wsMoney: 0
+          }
+          this.goodsData.forEach(item=>{
+            params[item.value] = 0
+            this.allGameData.forEach(data=>{
+              params[item.value] += Number(data[item.value] || 0)
+            })
+          })
+          this.allGameData.forEach(data=>{
+              if(data.hasBz){
+                params.bz++
+              }
+              if(data.money>=100){
+                params.bwMoney++
+              }
+              if(data.money<=25 && data.money>0){
+                params.ljMoney++
+              }
+              if(data.money>=50&&data.money<=70){
+                params.wsMoney++
+              }
+            })
+          return params
+        },
+
+        viewData(){
+          this.gameData = this.getGameData()
+          this.showData=true
+        },
+        resetData(){
+          this.allGameData=[]
+          for(let i=0;i<8;i++){
+            const params= {
+              hasBz: false,
+              money: 15
+            }
+            this.goodsData.forEach(item=>{
+              params[item.value] = 0
+            })
+            this.allGameData.push({...params})
+          }
         }
   }
 }
